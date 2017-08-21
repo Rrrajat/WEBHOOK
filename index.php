f2393be7f7ad977392cf752f6b19d3de078a5193
Rrrajat <31208762+Rrrajat@users.noreply.github.com>
2017-08-21 17:16:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,21 @@
 
 // your code goes here
 $method = $_SERVER['REQUEST_METHOD'];
-if($method == 'GET')
+if($method == 'POST')
 {
     $requestBody = file_get_contents('php://input');
     $json = json_decode($requestBody);
-    $text = $json->result->fulfillment->messages;
-    echo ($text + "test");
+    $text = $json->result->fulfillment->speech;
+    $speech=$text;
+    $respone=new \stdClass();
+    $respone->speech = $speech;
+    $respone->displayText=$speech;
+    $respone->source="webhook";
+    echo json_encode($respone);
 }
 
 else {
     echo "Method not allowed";
 }
 ?>
+
