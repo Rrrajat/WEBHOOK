dd6dd879d85c439b03aa4cb0ccd5e4abf445e33a
Rrrajat <Rrrajat@DESKTOP-UI826S5>
2017-08-23 02:38:24 +0530

    First Deploy

---
+++ Webhook/Webhook/Controllers/APIController.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webhook.Controllers
+{
+    public class APIController : Controller
+    {
+        // GET: API
+        public ActionResult Index()
+        {
+
+            if (HttpContext.Request.HttpMethod == "POST")
+            {
+                var requestBody = Request.InputStream;
+                var jsonResult = Json(requestBody, JsonRequestBehavior.AllowGet).ToString();
+                var jsonObject = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonResult);
+                var text = jsonObject["result"]["fulfillment"]["speech"];
+                var finalResult = new { speech = text, displayText = text, source = "webhook" };
+                var result = Json(finalResult);
+                ViewBag.result = result;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Method not allowed");
+                ViewBag.result = "Method";
+            }
+            return View();
+        }
+    }
+}
---
+++ Webhook/Webhook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Webhook.Startup))]
+namespace Webhook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
